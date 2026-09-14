96f86df80ddbba77717a83e50790d20287cc97ee
sparktution22-web <sparktution22@gmail.com>
2026-09-14 16:41:42 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ import AdminMarks from './pages/admin/AdminMarks.jsx'
 import AdminFees from './pages/admin/AdminFees.jsx'
 import AdminCreateAccount from './pages/admin/AdminCreateAccount.jsx'
 import AdminHomework from './pages/admin/AdminHomework.jsx'
+import AdminSendMessage from './pages/admin/AdminSendMessage.jsx'
 import AdminOnlineClasses from './pages/admin/AdminOnlineClasses.jsx'
 import AdminStudyMaterials from './pages/admin/AdminStudyMaterials.jsx'
 import AdminDoubtBox from './pages/admin/AdminDoubtBox.jsx'
@@ -143,6 +144,14 @@ function App() {
               element={
                 <ProtectedRoute roles={['admin']}>
                   <AdminHomework />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="admin/send-message"
+              element={
+                <ProtectedRoute roles={['admin']}>
+                  <AdminSendMessage />
                 </ProtectedRoute>
               }
             />
